51e0e549c840a015602ca39b15d3b2e1d7563809
830Utpal <lakshman2016yadav@gmail.com>
2025-03-15 19:02:54 +0530

    header.tsx

--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -11,3 +11,14 @@ return user;
         return null
     }
 }
+
+
+export async function getUserByEmail(email:string){
+    try{
+           const [user]=await db.select().from(Users).where(eq(Users.email,email)).execute()
+           return user;
+    }catch(error){
+         console.error('Error fetching user by email',error)
+         return null
+    }
+}

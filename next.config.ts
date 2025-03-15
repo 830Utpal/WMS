6cb97bcc41294f78b8cc1ca797c6d7cff63e4d5f
830Utpal <lakshman2016yadav@gmail.com>
2025-03-16 02:17:49 +0530

    page.tsx report

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+    env:{
+      DATABASE_URL:process.env.NEXT_PUBLIC_DATABASE_URL,
+      NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID:process.env.NEXT_PUBLIC_WEB3_AUTH_CLIENT_ID,
+      NEXT_PUBLIC_GEMINI_API_KEY:process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   },
 };
 
